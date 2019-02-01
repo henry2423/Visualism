335d31881dbf9d5c68576af488fb750d011b51dc
Henry Huang <henry2423@gmail.com>
2019-02-01 11:32:19 -0600

    [ADD] Testing and Travis config

---
+++ VisualismTests/VisualismTests.swift
@@ -0,0 +1,36 @@
+//
+//  VisualismTests.swift
+//  VisualismTests
+//
+//  Created by Henry Huang on 2/1/19.
+//  Copyright © 2019 Henry Huang. All rights reserved.
+//
+
+import XCTest
+//@testable import Pods_Visualism
+
+class VisualismTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
